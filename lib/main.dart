b1bc398cdc76313e9a3ef6d15ba2eb27169c72ec
Abhishek <joshi.abj13@gmail.com>
2018-12-05 01:29:40 +0530

    My first git commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(new MyApp());
+
+class RandomWordsState extends State<RandomWords> {
+  @override                                  // Add from this line ...
+  Widget build(BuildContext context) {
+    final WordPair wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          //Hi there
+          //child: new Text(wordPair.asPascalCase), // Change this line to...
+          child: new RandomWords(),                 // ... this line.
+        ),
+      ),
+    );
+  }
+}
